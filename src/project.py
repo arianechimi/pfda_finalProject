b01fc2e945d78a6d98dcf7e85f075608871c1be9
Ariane Chimi <arianechimi5@gmail.com>
2026-04-25 14:37:15 -0500

    create class for enemies and platform

--- src/project.py
+++ src/project.py
@@ -390,6 +390,47 @@ class World():
 			screen.blit(tile[0], tile[1])
 
 
+class Enemies(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load('images/enemy.png')
+		self.rect = self.image.get_rect(topleft=(x, y))
+		self.hitbox = self.rect.copy()
+		self.move_direction = 1
+		self.move_counter = 0
+ 
+	def update(self):
+		self.hitbox.x += self.move_direction
+		self.move_counter += 1
+		if abs(self.move_counter) > 50:
+			self.move_direction *= -1
+			self.move_counter *= -1
+		self.rect.x = self.hitbox.x
+		self.rect.y = self.hitbox.y
+
+
+class Platform(pygame.sprite.Sprite):
+	def __init__(self, x, y, move_x, move_y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('images/platform.png')
+		self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.move_direction = 1
+		self.move_counter = 0
+		self.move_x = move_x
+		self.move_y = move_y
+ 
+	def update(self):
+		self.rect.x += self.move_direction * self.move_x
+		self.rect.y += self.move_direction * self.move_y
+		self.move_counter += 1
+		if abs(self.move_counter) > 60:
+			self.move_direction *= -1
+			self.move_counter *= -1
+
+
 
 if __name__ == '__main__':
 	main()
