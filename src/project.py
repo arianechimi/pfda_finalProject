d59b7590769acb9eb8f84415683f444bb7f997fd
Ariane Chimi <arianechimi5@gmail.com>
2026-04-25 10:01:47 -0500

    Set up, music, frame rate, and screen size

--- src/project.py
+++ src/project.py
@@ -1,3 +1,16 @@
 import pygame
 from pygame.locals import *
 from pygame import mixer
+
+pygame.mixer.pre_init(44100, -16, 2, 512)
+mixer.init()
+pygame.init()
+ 
+clock = pygame.time.Clock()
+fps = 60
+ 
+screen_width = 1000
+screen_height = 1000
+ 
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Get out')
